4a29ac2f89f3a6c172ce60d47f55d2e50f95379e
xxx508 <khabarov_roman@mail.ru>
2024-03-25 08:59:26 +0300

    fckk
    Former-commit-id: 89451df4b20859df27bdfaf299339a29a7e0b7b9

---
+++ 04/SoftwareToolsInfoSec/01/md5create/md5create/.vshistory/Program.cs/2024-03-21_07_40_11_559.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите имя файла:");
+        string fileName = Console.ReadLine();
+
+        if (File.Exists(fileName))
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    byte[] hash = MD5.Create().ComputeHash(File.OpenRead(fileName));
+                    byte[] hash = md5.ComputeHash(stream);
+                    string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+                    Console.WriteLine($"MD5 хеш файла {fileName}: \n{hashString}");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+    }
+}
---
+++ 04/SoftwareToolsInfoSec/01/md5create/md5create/.vshistory/Program.cs/2024-03-21_07_40_30_178.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите имя файла:");
+        string fileName = Console.ReadLine();
+
+        if (File.Exists(fileName))
+        {
+
+            byte[] hash = MD5.Create().ComputeHash(File.OpenRead(fileName));
+            string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+            Console.WriteLine($"MD5 хеш файла {fileName}: \n{hashString}");
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+    }
+}
