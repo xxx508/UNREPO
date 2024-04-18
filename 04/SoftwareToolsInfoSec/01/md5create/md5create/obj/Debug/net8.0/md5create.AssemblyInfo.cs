e27c22a84a7b97379cf69e1fc3c398f1ef64a85d
xxx508 <khabarov_roman@mail.ru>
2024-04-18 06:19:21 +0300

    fck
    Former-commit-id: a4d45f2884cf1a3bfc02ee13d3dd2ffbe0f0ca76

--- 04/SoftwareToolsInfoSec/01/md5create/md5create/obj/Debug/net8.0/md5create.AssemblyInfo.cs
+++ 04/SoftwareToolsInfoSec/01/md5create/md5create/obj/Debug/net8.0/md5create.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("md5create")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec1d887b694166762a0ab55fcfb26c892f20b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+752f4aa1172aa9569d5cb14b4a877e28d25d613e")]
 [assembly: System.Reflection.AssemblyProductAttribute("md5create")]
 [assembly: System.Reflection.AssemblyTitleAttribute("md5create")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 04/SoftwareToolsInfoSec/02/retrieveregistryinfo/retrieveregistryinfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Win32;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string serialNumber = "";
+        string motherboardName = "";
+
+        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS"))
+        {
+            if (key != null)
+            {
+                serialNumber = key.GetValue("SystemSKU").ToString();
+            }
+        }
+
+        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\BIOS"))
+        {
+            if (key != null)
+            {
+                motherboardName = key.GetValue("BaseBoardProduct").ToString();
+            }
+        }
+
+        string filePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Desktop\\system_info.txt";
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("Серийный номер: " + serialNumber);
+            writer.WriteLine("Название мат. платы: " + motherboardName);
+        }
+
+        Console.WriteLine("Информация была сохранена в " + filePath);
+    }
+}
